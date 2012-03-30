b105b39bd51caf2a41a3c134aec7fc4befa256bc
Alex Joyal <ajoyal@student.umass.edu>
2012-03-30 14:19:03 -0400

    board init fixed

--- public/javascripts/othello.js
+++ public/javascripts/othello.js
@@ -9,12 +9,13 @@ function checkGameOver(){
 
 }
 
-function GameMove(){
-	availablePlays = getAvailablePlays();
+function gameMove(){
 
-	if (//availablePlays.contains(this.className)){
+	availablePlays = gameBoard; //getAvailablePlays();
+
+	if (availablePlays.contains(this.className)){
 		this.className = player.name;
-		game.plays.add({player: this.id);
+		//game.plays.add({player: this.id);
 		               
 		
 
@@ -27,17 +28,44 @@ function GameMove(){
 	}
 }
 function setUpBoard(){
-      for (var j = 0; j < 10; j++){
-      	for(var i = 0; i < 10; i++){
-	        gameboard[j] = document.getElementById( j.toString() + i.toString() );
-	        gameboard[j].onclick = GameMove;
-         }
-      }
-   }
+    for (var j = 11; j < 18; j++){
+        gameboard[j] = document.getElementById( j.toString());
+        gameboard[j].onclick = gameMove;
+    }
+    for (var j = 21; j < 28; j++){
+        gameboard[j] = document.getElementById( j.toString());
+        gameboard[j].onclick = gameMove;
+    }
+    for (var j = 31; j < 38; j++){
+        gameboard[j] = document.getElementById( j.toString());
+        gameboard[j].onclick = gameMove;
+    }
+    for (var j = 41; j < 48; j++){
+        gameboard[j] = document.getElementById( j.toString());
+        gameboard[j].onclick = gameMove;
+    }
+    for (var j = 51; j < 58; j++){
+        gameboard[j] = document.getElementById( j.toString());
+        gameboard[j].onclick = gameMove;
+    }
+    for (var j = 61; j < 68; j++){
+        gameboard[j] = document.getElementById( j.toString());
+        gameboard[j].onclick = gameMove;
+    }
+    for (var j = 71; j < 78; j++){
+        gameboard[j] = document.getElementById( j.toString());
+        gameboard[j].onclick = gameMove;
+    }
+    for (var j = 81; j < 88; j++){
+        gameboard[j] = document.getElementById( j.toString());
+        gameboard[j].onclick = gameMove;
+    }
+}
 
 //initalize game and board
 function initializeGame(){
 	var gameboard =  [];
+	setUpBoard();
 
 }
 
