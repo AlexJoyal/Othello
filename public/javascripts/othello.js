694c69dbd5e640c02ba4ac8c22cd629993735726
Alex Joyal <ajoyal@student.umass.edu>
2012-03-30 15:32:04 -0400

    i hope this works

--- public/javascripts/othello.js
+++ public/javascripts/othello.js
@@ -1,70 +1,61 @@
 
 //check for game over conditions
+
+
+//initalize game and board
+function initializeGame(){
+
 function checkGameOver(){
-	//no more possible moves
-	//board is complete
-	//all tiles are one color.
+    //no more possible moves
+    //board is complete
+    //all tiles are one color.
 
 
 
 }
 
-function gameMove(gameboard){
+function gameMove(){
 
-	availablePlays = getAvailablePlays(gameboard);
+    var availablePlays = getAvailablePlays();
 
-	if (availablePlays.contains(this.className)){
-		this.className = player;
-		
+    if (availablePlays[this.id]){
+        executeMove(this.id, gameboard);
+        
         //switch players turn
         if (player == player1){
             player = player2;
         } else {
             player = player1;
         }
-	}
+    }
 
 }
-function setUpBoard(gameboard){
-    for (var j = 11; j < 18; j++){
-        gameboard[j] = document.getElementById( j.toString());
-        gameboard[j].onclick = gameMove;
+
+function getAvailablePlays(){
+    return gameboard;
+    //implement this!
+}
+
+
+
+function setUpBoard(){
+    for (var j = 11; j < 88; j++) {
+        if ( (j%10 != 9) && (j%10 != 0) ) { 
+            gameboard[j] = document.getElementById( j.toString());
+            gameboard[j].onclick = gameMove;
+        }
     }
-    for (var j = 21; j < 28; j++){
-        gameboard[j] = document.getElementById( j.toString());
-        gameboard[j].onclick = gameMove;
-    }
-    for (var j = 31; j < 38; j++){
-        gameboard[j] = document.getElementById( j.toString());
-        gameboard[j].onclick = gameMove;
-    }
-    for (var j = 41; j < 48; j++){
-        gameboard[j] = document.getElementById( j.toString());
-        gameboard[j].onclick = gameMove;
-    }
-    for (var j = 51; j < 58; j++){
-        gameboard[j] = document.getElementById( j.toString());
-        gameboard[j].onclick = gameMove;
-    }
-    for (var j = 61; j < 68; j++){
-        gameboard[j] = document.getElementById( j.toString());
-        gameboard[j].onclick = gameMove;
-    }
-    for (var j = 71; j < 78; j++){
-        gameboard[j] = document.getElementById( j.toString());
-        gameboard[j].onclick = gameMove;
-    }
-    for (var j = 81; j < 88; j++){
-        gameboard[j] = document.getElementById( j.toString());
-        gameboard[j].onclick = gameMove;
-    }
+
     for (var j = 89; j < 100; j++){
-    	gameboard[j] = undefined;
+        gameboard[j] = undefined;
     }
+
+    gameboard[44].className = player1;
+    gameboard[55].className = player1;
+    gameboard[45].className = player2;
+    gameboard[54].className = player2;
 }
 
-//initalize game and board
-function initializeGame(){
 	var player1 = "player1";
 	var player2 = "player2"
 	var player = player1;
@@ -72,5 +63,12 @@ function initializeGame(){
 	setUpBoard(gameboard);
 
 }
+
+function executeMove(id, gameboard){
+    while (true){
+        if (gameboard[id])
+    }
+}
+
 
 window.onload = initializeGame();
