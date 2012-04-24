a925af07c00ec7f561d3f2e464e09dc02e4f6a82
Alex Joyal <ajoyal@student.umass.edu>
2012-04-24 10:10:29 -0400

    added some login logout methods

--- app.js
+++ app.js
@@ -5,9 +5,7 @@
 
 var express = require('express')
   , routes = require('./routes');
- // , io  = require('socket.io');
 
-//io.set('log level', 1);
 
 var app = module.exports = express.createServer();
 
@@ -18,6 +16,7 @@ app.configure(function(){
   app.set('view engine', 'ejs');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.session({ secret: 'cs391wp' }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -37,35 +36,17 @@ app.get('/othello', routes.playOthello);
 app.get('/newuser', routes.newuser);
 app.post('/createuser', routes.createuser);
 app.post('/savegame', routes.savegame);
+app.post('/login', routes.login);
+app.get('/login', function(req, res){
+    var loggedIn = req.session.user;
+    //console.log(loggedIn);
+    //redirect to /transactions if theres is a valid session
+    if (loggedIn){
+        res.redirect('/othello');
+        return;
+    }
+    res.render('login', {title: 'User Login', error: ""});
+});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-/*
-io.sockets.on('connection',
-              function (socket) {
-               
-
-
-                  // Send back to client
-                  socket.set('id', bid, function () {
-                      var msg = { you  : block,
-                                  them : blocks };
-                      socket.emit('block', msg);
-                  });
-
-                  // Send to other clients
-                  socket.broadcast.emit('+block', block);
-
-                  // Register move events:
-                  socket.on('save', function (userid, gameboard) {
-                      //save gameboard
-                  });
-
-                  socket.on('disconnect', function () {
-                      socket.get('id', function (err, id) {
-                          var block = blocks[id];
-                          delete blocks[id];
-                          socket.broadcast.emit('-block', block);
-                      });
-                  });
-              });*/
--- routes/index.js
+++ routes/index.js
@@ -67,6 +67,35 @@ exports.loadgame = function(req, res){
 	
 }
 
+exports.login = function(req, res) {
+    // TODO: user login
+  
+	var name = req.body.name;
+	var password = req.body.password;
+	var user = {
+		'name': name,
+		'pass': password
+	}
+	//redirect to /create if user is undefined
+	db.getUser(user, function(err, result){
+			if (err){
+			    res.render('login', {title: 'User Login', error: "Invalid username or password"});
+			} else {
+				req.session.user = result.rows[0].uid;
+				res.redirect('/othello');
+				return;
+			}
+
+	});
+	//res.render('login', {title: 'User Login', error: "Invalid username or password"});
+};
+
+exports.logout = function(req, res) {
+    // TODO: user logout
+    req.session.destroy();
+    res.redirect('/login');
+};
+
 function checkUserExists(email){
 	db.getUserByEmail(email, function(err, result){
 		if(err){
