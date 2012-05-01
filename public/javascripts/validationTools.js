4617182741372f702e0bbc7a2abdcc220b8a3812
Alex Joyal <ajoyal@student.umass.edu>
2012-05-01 09:15:58 -0400

    changed validationTools

--- public/javascripts/validationTools.js
+++ public/javascripts/validationTools.js
@@ -2,7 +2,7 @@ function validateCreateForm() {
     var name = document.forms["createuser"]["name"].value;
     var pass = document.forms["createuser"]["password"].value;
     var email = document.forms["createuser"]["email"].value;
-    if (name == null || pass == "" || email == null){
+    if (name == "" || pass == "" || email == ""){
         alert("All fields must be filled out");
         return false;
     }
@@ -11,7 +11,7 @@ function validateCreateForm() {
 function validateLoginForm() {
     var name = document.forms["loginForm"]["name"].value;
     var pass = document.forms["loginForm"]["password"].value;
-    if (name == null || pass == ""){
+    if (name == "" || pass == ""){
         alert("All fields must be filled out");
         return false;
     }
